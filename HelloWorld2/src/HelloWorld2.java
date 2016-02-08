1a9cb793b73bf345a756f6b1df7f7382affd2be1
Robert Balchunas <robert.balchunas@maine.edu>
2016-02-08 08:05:27 -0500

    For an update

--- HelloWorld2/src/HelloWorld2.java
+++ HelloWorld2/src/HelloWorld2.java
@@ -1,6 +1,9 @@
 
 public class HelloWorld2 {
 	public static void main(String[] args){
+		System.out.println("Here ye Here ye : important announcement");
 		System.out.println("Hello GIT");
+		System.out.println("Goodbye");
+		
 	}
 }
